5b364e126e1898a88395ee7b14a0181dfc5f0d6a
benjaminlesieux <benjamin.lesieux@efrei.net>
2023-06-14 10:14:52 +0200

    first commit <3

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Image height={120} width={120} src={"/doteasedocs_logo.svg"} alt={"dotease logo"}/>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dotease/dotease',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/dotease/dotease',
   footer: {
     text: 'Nextra Docs Template',
   },
